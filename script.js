c4444b3d1f662d444b83dc97ebcbe871b09667cc
showboat051 <rashad.davis87@yahoo.com>
2022-01-12 23:58:22 -0600

    fetch needs work

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function fetchWeather() {
-   fetch('api.openweathermap.org/data/2.5/weather?q=London&appid=94f3f05eb827c75e7211a374d0a92167').then(res => {
-       console.log(res);
-   })
-}
+// // function fetchWeather() {
+// //    fetch('api.openweathermap.org/data/2.5/weather?q=London&appid=94f3f05eb827c75e7211a374d0a92167').then(Response => {
+// //        console.log(Response);
+// //    })
+// // }
 
-fetchWeather();
+// // fetchWeather();
+// alert('heyae')
+
+
+// fetch(`api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`)
+// .then(Response => {
+//     return Response.json();
+// })
+// .then(Response => {
+//     console.log(Response)
+    
+// })
+
+let weather = {
+     apiKey : '94f3f05eb827c75e7211a374d0a92167',
+     getWeather: function(city) {
+         fetch("api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey)
+         .then((response) => response.json())
+         .then((data) => console.log(data));
+        }
+    }
