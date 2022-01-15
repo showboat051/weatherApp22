67b2ecc52011850c4ba137996667e377da14bc6f
showboat051 <rashad.davis87@yahoo.com>
2022-01-15 00:16:33 -0600

    added button

--- script.js
+++ script.js
@@ -17,11 +17,45 @@
     
 // })
 
-let weather = {
-     apiKey : '94f3f05eb827c75e7211a374d0a92167',
-     getWeather: function(city) {
-         fetch("api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey)
-         .then((response) => response.json())
-         .then((data) => console.log(data));
-        }
-    }
+// let weather = {
+//      apiKey : '94f3f05eb827c75e7211a374d0a92167',
+//      getWeather: function(city) {
+//          fetch("api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey)
+//          .then((response) => response.json())
+//          .then((data) => console.log(data));
+//         }
+//     }
+
+    // let weather = {
+    //     apiKey : '94f3f05eb827c75e7211a374d0a92167',
+    //     getWeather: function(city) {
+    //         fetch("api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey)
+    //         .then(async Response => {
+    //             try {
+    //                 const data = await Response.json()
+    //                 console.log('response data', data);
+    //             }
+    //         }) finally{
+                
+    //         }
+    //        } // getWeather()
+    //    } // weather object
+   
+// let weather = {
+//     apiKey : '94f3f05eb827c75e7211a374d0a92167',
+//     getWeather: async function(city) {
+//         const url =`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         console.log(data);
+//     }
+// }
+
+async function getWeather(city) {
+    const apiKey = "94f3f05eb827c75e7211a374d0a92167";
+    const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    JSON.parse(JSON.stringify(userData))
+    console.log(data);
+}
