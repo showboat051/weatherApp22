f3e0fdd74302eded3ab25f96a26f3a12c543356f
showboat051 <rashad.davis87@yahoo.com>
2022-02-01 01:09:10 -0600

    url is refined

--- script.js
+++ script.js
@@ -1,87 +1,54 @@
-// function getWeather() {
-//    fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dallas?unitGroup=metric&include=current&key=QVYL5LAZMZWQR3BM5H77WSZ3P&contentType=json').then(Response => {
-//        Response.json()
-//        console.log('resolved' , Response);
-//    }).catch((err) => {
-//        console.log('rejected', err);
-//    })
-// }
-
-// // fetchWeather();
-// alert('heyae')
-
 function getWeather() {
-    fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dallas?unitGroup=metric&include=current&key=QVYL5LAZMZWQR3BM5H77WSZ3P&contentType=json')
-    .then(res => res.json())
-    .then(data => console.log(data));
+        const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
+
+   fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dallas?unitGroup=metric&include=current&key=${apiKey}&contentType=json`).then(Response => {
+      return Response.json();
+       console.log( Response);
+   }).then ( data => {
+       console.log(data);
+   })
+   
 }
 
+// fetchWeather();
 
-
-// fetch(`api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`)
-// .then(Response => {
-//     return Response.json();
-// })
-// .then(Response => {
-//     console.log(Response)
+// function getWeather() {
     
-// })
+//     // my request
+//     const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
+//     // fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dallas?unitGroup=metric&include=current&key=QVYL5LAZMZWQR3BM5H77WSZ3P&contentType=json')
+//     // .then(res => res.json())
+//     // .then(data => console.log(data));
 
-// let weather = {
-//      apiKey : '94f3f05eb827c75e7211a374d0a92167',
-//      getWeather: function(city) {
-//          fetch("api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey)
-//          .then((response) => response.json())
-//          .then((data) => console.log(data));
+//     // example request
+//     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New%20York%20City%2CNY?unitGroup=us&key=${apiKey}&contentType=json`, {
+//         method: 'GET', 
+//         headers: {
+       
+//         },
+                 
+//       }).then(response => {
+//         if (!response.ok) {
+//             console.log(response);
+//           throw response; //check the http response code and if isn't ok then throw the response as an error
 //         }
-//     }
-
-    // let weather = {
-    //     apiKey : '94f3f05eb827c75e7211a374d0a92167',
-    //     getWeather: function(city) {
-    //         fetch("api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey)
-    //         .then(async Response => {
-    //             try {
-    //                 const data = await Response.json()
-    //                 console.log('response data', data);
-    //             }
-    //         }) finally{
-                
-    //         }
-    //        } // getWeather()
-    //    } // weather object
-   
-// let weather = {
-//     apiKey : '94f3f05eb827c75e7211a374d0a92167',
-//     getWeather: async function(city) {
-//         const url =`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-//         const response = await fetch(url);
-//         const data = await response.json();
-//         console.log(data);
-//     }
-// }
-
-// async function getWeather(city) {
-//     const apiKey = "94f3f05eb827c75e7211a374d0a92167";
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     // JSON.parse(JSON.stringify(data))
-//     console.log(data);
-// }
-
-// const ft = new FetchWeather();
-
-// const city = document.getElementById("cityBox");
-// const button = document.getElementById("submit");
-// button.addEventListener( "click" , () => {
-//     const currentCity = city.value;
-
-//     ft.getWeather(currentCity).then ((data) => {
-//         console.log(data);
-//     })
-// })
-
+                  
+//         return response.json(); //parse the result as JSON
+      
+//       }).then(response => {
+//         //response now contains parsed JSON ready for use
+//         processWeatherData(response);
+      
+//       }).catch((errorResponse) => {
+//         if (errorResponse.text) { //additional error information
+//           errorResponse.text().then( errorMessage => {
+//             //errorMessage now returns the response body which includes the full error message
+//           })
+//         } else {
+//           //no additional error information 
+//         } 
+//       });
+// } // getWeather()
 
 
 
