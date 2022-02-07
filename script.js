cfeeca02b6d5e684516b0b2e752e0a1c0b06090c
showboat051 <rashad.davis87@yahoo.com>
2022-02-06 23:00:05 -0600

    url is not dynamic

--- script.js
+++ script.js
@@ -1,57 +1,28 @@
-function getWeather(cityBox) {
-        const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
+//**************************RESOURCES************************************* */
 
-   fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=${apiKey} `).then(Response => {
-      return Response.json();
-   }).then ( data => {
+const weatherInfoDiv = document.getElementById("weatherInfo");
+const weatherDescDiv = document.getElementById("weatherDesc");
+
+function getWeather() {
+   
+    const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
+    
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey} `).then(Response => {
+        return Response.json();
+    }).then ( data => {
+  
        console.log(data);
+       
        const weather = data.currentConditions.temp;
-       const weatherInfoDiv = document.getElementById("weatherInfo");
-       weatherInfoDiv.innerHTML = "The current temperature is " + weather;
+       const dayDesc = data.description;
+       weatherInfoDiv.innerHTML = "The current temperature is " + weather + " with " + dayDesc;
+       
+       weatherDescDiv.innerHTML = dayDesc;
    }).catch(error => {
        console.log(error)
    });
 } // getWeather()
 
-// fetchWeather();
-
-// function getWeather() {
-    
-//     // my request
-//     const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
-//     // fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dallas?unitGroup=metric&include=current&key=QVYL5LAZMZWQR3BM5H77WSZ3P&contentType=json')
-//     // .then(res => res.json())
-//     // .then(data => console.log(data));
-
-//     // example request
-//     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New%20York%20City%2CNY?unitGroup=us&key=${apiKey}&contentType=json`, {
-//         method: 'GET', 
-//         headers: {
-       
-//         },
-                 
-//       }).then(response => {
-//         if (!response.ok) {
-//             console.log(response);
-//           throw response; //check the http response code and if isn't ok then throw the response as an error
-//         }
-                  
-//         return response.json(); //parse the result as JSON
-      
-//       }).then(response => {
-//         //response now contains parsed JSON ready for use
-//         processWeatherData(response);
-      
-//       }).catch((errorResponse) => {
-//         if (errorResponse.text) { //additional error information
-//           errorResponse.text().then( errorMessage => {
-//             //errorMessage now returns the response body which includes the full error message
-//           })
-//         } else {
-//           //no additional error information 
-//         } 
-//       });
-// } // getWeather()
 
 
 
