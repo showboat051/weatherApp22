a1fc1af099471e0e6c226186a6b1a43fe97cc5dd
showboat051 <rashad.davis87@yahoo.com>
2022-02-10 01:26:21 -0600

    url is dynamic but not working yet

--- script.js
+++ script.js
@@ -2,15 +2,14 @@
 
 const weatherInfoDiv = document.getElementById("weatherInfo");
 const weatherDescDiv = document.getElementById("weatherDesc");
+const city = document.getElementById("cityBox");
 
 function getWeather() {
-   
-    const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
-    
-    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey} `).then(Response => {
-        return Response.json();
-    }).then ( data => {
-  
+        const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
+       
+   fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey} `).then(Response => {
+      return Response.json();
+   }).then ( data => {
        console.log(data);
        
        const weather = data.currentConditions.temp;
