46ebcb35dd2441d3d941ff6d41c18fde976f39d3
showboat051 <rashad.davis87@yahoo.com>
2022-02-17 00:56:57 -0600

    JSON needs tidying

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 
 const weatherInfoDiv = document.getElementById("weatherInfo");
 const weatherDescDiv = document.getElementById("weatherDesc");
-const city = document.getElementById("cityBox");
+const city = document.getElementById("cityBox").value;
 
 function getWeather() {
         const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
        
-   fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey} `).then(Response => {
-      return Response.json();
-   }).then ( data => {
+   fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey} `).then(function (response) {
+       JSON.stringify(response)
+      return response.json();
+   }).then ( function (data) {
        console.log(data);
        
        const weather = data.currentConditions.temp;
