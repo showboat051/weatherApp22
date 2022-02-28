d0aeaf029a58d1a27cd1b7478b5c69544984740a
showboat051 <rashad.davis87@yahoo.com>
2022-02-28 00:50:52 -0600

    may have to use coordinates

--- script.js
+++ script.js
@@ -6,11 +6,23 @@ const city = document.getElementById("cityBox").value;
 
 function getWeather(city) {
         const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
+        if (city === '') {
+            alert("enter something")
+        }
        
    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`) 
-   .then((data) => data.json())
-   
-   .then((data) => console.log(data));
+
+   .then(res => {
+    if(res.ok) {
+        console.log('got it');
+        console.log(res);
+        return res.json()
+    } else {
+        console.log('nope');
+    }
+})
+.then(data => console.log(data))
+.catch(error => console.log('error'))
    
     
     // const weather = data.currentConditions.temp;
