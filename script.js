c41ec7a5c33e7240cc5c67a039696cf671bf1969
showboat051 <rashad.davis87@yahoo.com>
2022-03-05 00:03:48 -0600

    will rewrite whole fetch

--- script.js
+++ script.js
@@ -4,25 +4,28 @@ const weatherInfoDiv = document.getElementById("weatherInfo");
 const weatherDescDiv = document.getElementById("weatherDesc");
 const city = document.getElementById("cityBox").value;
 
-function getWeather(city) {
+function getWeather() {
         const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
-        if (city === '') {
+        if (city == '') {
             alert("enter something")
         }
-       
-   fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`) 
 
-   .then(res => {
-    if(res.ok) {
-        console.log('got it');
-        console.log(res);
-        return res.json()
-    } else {
-        console.log('nope');
-    }
-})
-.then(data => console.log(data))
-.catch(error => console.log('error'))
+        
+        fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`) 
+        
+        .then(res => {
+            if(res.ok) {
+                console.log(res);
+                return res.json()
+            } else {
+                console.log('nope');
+            }
+        })
+        .then(data =>
+            console.log(data)),
+            console.log("The city is " + city);
+       
+// .catch(error => console.log('error'))
    
     
     // const weather = data.currentConditions.temp;
