db03aaf1b345119913616fe226579e79f21405b8
showboat051 <rashad.davis87@yahoo.com>
2022-03-31 01:06:47 -0500

    adding 7 day forecast

--- script.js
+++ script.js
@@ -2,37 +2,53 @@
 
 const weatherInfoDiv = document.getElementById("weatherInfo");
 const weatherDescDiv = document.getElementById("weatherDesc");
-// const city = document.querySelector("cityBox").value;
+const goButton = document.getElementById("submit");
+
+//*******************ENTER BUTTON****************************/
+goButton.addEventListener("keyup" , function(e) {
+    if(e.key == 'Enter') {
+        e.preventDefault();
+        getWeather();
+        
+    }
+})
 
 function getWeather() {
+    
+    
     const city = document.getElementById("cityBox").value;
-        const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
-        if (city.value == ' ') {
-            alert("enter something")
-        }
-
-        
-        fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`) 
-        
-        .then(res => {
-            if(res.ok) {
-                return res.json()
-            } else {
+    const apiKey = 'QVYL5LAZMZWQR3BM5H77WSZ3P';
+    if (city.value == ' ') {
+        alert("enter something")
+    }
+    
+    
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`) 
+    
+    .then(res => {
+        if(res.ok) {
+            return res.json()
+        } else {
                 console.log('nope');
             }
         })
         .then(data => {
             console.log(data)
             var currentTemp = data.currentConditions.temp;
-            weatherInfoDiv.innerHTML = "The current temperature here in " + city + " is " + currentTemp;  
-        } 
+            weatherInfoDiv.innerHTML = "The current temperature here in " + city + " is " + currentTemp + " degrees ";  
+            weatherDescDiv.innerHTML = data.description
 
+            var table = document.createElement("table");
+            const sevenDay = document.getElementById("sevenDay");
+            sevenDay.innerHTML = data.days[0].temp
+        } 
+        
         )} // getWeather()
         
        
-       
-       
-       
+        
+        
+        
        
        
         function showWeather(data) {
